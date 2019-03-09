930c16b0a76eb21b4499acf0455688e34bb29917
Mikhail Morozov <mikhail.morozov@team.wrike.com>
2019-03-09 20:52:01 +0300

    layout

---
+++ lib/src/components/login/login.dart
@@ -0,0 +1,14 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'login',
+  styleUrls: ['login.css'],
+  templateUrl: 'login.html',
+  directives: [],
+  providers: [],
+  preserveWhitespace: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+)
+class LoginComponent {
+
+}
