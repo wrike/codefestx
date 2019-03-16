154338cfe74b80909d11f208384793f11b0cad5e
Dmitry Podkorytov <dmitry.podkorytov@gmail.com>
2019-03-16 12:53:58 +0300

    sync models with backend

--- lib/src/models/location.g.dart
+++ lib/src/models/location.g.dart
@@ -10,7 +10,7 @@ Location _$LocationFromJson(Map<String, dynamic> json) {
   return Location(
       id: json['id'] as String,
       title: json['title'] as String,
-      logoPath: json['logoPath'] as String,
+      iconPath: json['iconPath'] as String,
       mapPath: json['mapPath'] as String,
       description: json['description'] as String);
 }
@@ -18,7 +18,7 @@ Location _$LocationFromJson(Map<String, dynamic> json) {
 Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
       'id': instance.id,
       'title': instance.title,
-      'logoPath': instance.logoPath,
+      'iconPath': instance.iconPath,
       'mapPath': instance.mapPath,
       'description': instance.description
     };
