9a48be2b9bb5c629922b78917ee60a4e66519cca
Sinegovsky Ivan <ivan.sinegovsky@team.wrike.com>
2019-03-17 21:00:12 +0300

    fix race

--- lib/src/services/auth_store.dart
+++ lib/src/services/auth_store.dart
@@ -1,6 +1,10 @@
+import 'dart:html';
+
+import 'package:codefest/src/services/auth_service.dart';
+
 class AuthStore {
-  String token;
-  String userName;
+  String get token => window.localStorage[AuthService.tokenStorageKey];
+  String get userName => window.localStorage[AuthService.userNameStorageKey];
 
   bool get isAuth => token?.isNotEmpty ?? false;
 }
