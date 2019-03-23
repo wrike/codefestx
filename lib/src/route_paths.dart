2067df9cf1c366ecc8d47aa21371becaf0c4f7b3
Mikhail Morozov <mikhail.morozov@team.wrike.com>
2019-03-23 13:50:39 +0300

    new pages

--- lib/src/route_paths.dart
+++ lib/src/route_paths.dart
@@ -13,18 +13,18 @@ class RoutePaths {
   // Menu route paths
   static final lectures = MenuRoutePath(title: 'Расписание', path: 'lectures');
   static final rating = MenuRoutePath(title: 'Рейтинг докладов', path: 'rating');
-  static final speakers = MenuRoutePath(title: 'Спикеры', path: 'speakers');
   static final map = MenuRoutePath(title: 'Карта', path: 'map');
-  static final feedback = MenuRoutePath(title: 'Отправить feedback', path: 'feedback');
+  static final release_notes = MenuRoutePath(title: 'Что нового', path: 'release_notes');
+  static final about = MenuRoutePath(title: 'О нас', path: 'about');
   static final login = MenuRoutePath(title: 'Войти', path: 'login');
 
   static final List<MenuRoutePath> _menu = [
     lectures,
     rating,
-    speakers,
     map,
+    release_notes,
+    about,
     login,
-    feedback,
   ];
 
   static List<MenuRoutePath> get menu => _menu;
