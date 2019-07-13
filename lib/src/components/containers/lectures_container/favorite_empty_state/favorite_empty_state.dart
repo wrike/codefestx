d3cc40eb05222f61eb72ea5bfb6529f8292a0fd8
Dmitry Podkorytov <dmitry.podkorytov@team.wrike.com>
2019-07-13 18:28:19 +0300

    Update Favorites empty state

--- lib/src/components/containers/lectures_container/favorite_empty_state/favorite_empty_state.dart
+++ lib/src/components/containers/lectures_container/favorite_empty_state/favorite_empty_state.dart
@@ -5,7 +5,7 @@ import 'package:codefest/src/components/ui/empty_state/empty_state.dart';
   selector: 'favorite-empty-state',
   templateUrl: 'favorite_empty_state.html',
   directives: [
-    EmptyStateComponent
+    EmptyStateComponent,
   ],
   preserveWhitespace: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
