935181cd57cf72bcf5f638c20c39f649c26daf16
Roman Chekurov <roman.chekurov@team.wrike.com>
2019-07-15 17:04:31 +0300

    Fix zero state position

--- lib/src/components/containers/lecture_talk/empty_state/talks_zero_state.dart
+++ lib/src/components/containers/lecture_talk/empty_state/talks_zero_state.dart
@@ -4,6 +4,7 @@ import 'package:codefest/src/components/ui/empty_state/empty_state.dart';
 @Component(
   selector: 'talks-zero-state',
   templateUrl: 'talks_zero_state.html',
+  styleUrls: ['talks_zero_state.css'],
   directives: [
     EmptyStateComponent,
   ],
